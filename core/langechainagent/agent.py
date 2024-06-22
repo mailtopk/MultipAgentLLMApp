6837ef4a586d35826e9f935a19dec0e11825241b
mailtopk <ppkumar.email@gmail.com>
2024-06-22 16:16:47 -0700

    WIP:Added code to support memory for agents

--- core/langechainagent/agent.py
+++ core/langechainagent/agent.py
@@ -4,29 +4,39 @@ import operator
 from typing import TypedDict, Annotated
 from langchain_core.messages import AnyMessage
 
+from langgraph.checkpoint.sqlite import SqliteSaver
+
+
 class AgentState(TypedDict):
     messages: Annotated[list[AnyMessage], operator.add]
 
 class Agent:
-    def __init__(self, model, tools, system="") -> None:
+    def __init__(self, model, tools,system="") -> None:
         self.system = system
         
-        graph = StateGraph(AgentState)
-        graph.add_node("llm", self.call_openai)
-        graph.add_node("action", self.take_action)
-        graph.add_conditional_edges(
+        state_graph = StateGraph(AgentState)
+        state_graph.add_node("llm", self.call_openai)
+        state_graph.add_node("action", self.take_action)
+        state_graph.add_conditional_edges(
             "llm", self.exists_action,
             {True: "action", False: END}
         )
 
-        graph.add_edge("action", "llm")
-        graph.set_entry_point("llm")
-        self.graph = graph.compile()
+        state_graph.add_edge("action", "llm")
+        state_graph.set_entry_point("llm")
+
+        memory = SqliteSaver.from_conn_string(":memory:")
+
+        self.graph = state_graph.compile(checkpointer=memory, debug=True)
         self.tools = {t.name: t for t in tools}
+        # bind tools to llm model
         self.model = model.bind_tools(tools)
+
+        
 
     # llm node
     def call_openai(self, state:AgentState):
+        print("Calling LLM Model")
         messages = state['messages']
         if self.system:
             messages = [SystemMessage(content=self.system)] + messages
@@ -38,16 +48,16 @@ class Agent:
     def take_action(self, state: AgentState):
         tool_calls = state['messages'][-1].tool_calls
         results = []
-        for t in tool_calls:
-            print(f"calling tool {t}")
-            if not t['name'] in self.tools: # check if tool is bad from LLM
+        for tool in tool_calls:
+            print(f"calling tool id {tool['id']} , Name {tool['name']} with arguments {tool['args']}")
+            if not tool['name'] in self.tools: # check if tool is bad from LLM
                 print("\n...bad tool name..") 
-                retsult = "bad tool name, retry"  # instruct LLM to retry if bad
+                result = "bad tool name, retry"  # instruct LLM to retry if bad
             else:
-                result = self.tools[t['name']].invoke(t['args'])
-            results.append(ToolMessage(tool_call_id=t['id'], name=t['name'], content=str(result)))
+                result = self.tools[tool['name']].invoke(tool['args'])
+            results.append(ToolMessage(tool_call_id=tool['id'], name=tool['name'], content=str(result)))
 
-        print('Back to the model!')
+        print('Action Node completed, Back to the model!')
         return {'messages': results}
 
     def exists_action(self, state: AgentState):
